674c5541118dfec4c0a7bdae82a0db8d3b28483a
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-26 12:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ 
+function updateTimer() {
+  const currentDate = new Date();
+  const formattedDateTime = currentDate.toLocaleString();
+  document.getElementById('timer').textContent = formattedDateTime;
+}
+ 
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.innerHTML = "<p id='timer'></p>"
+  updateTimer();
+  setInterval(updateTimer, 1000);
+});
